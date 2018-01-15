d36023c4ba3a2f3be8f156dd4f6929f3984f64fd
Hiep Nguyen <yzmism@gmail.com>
2018-01-14 22:54:55 -0800

    Change export for ZSH

--- oh-my-zsh.zsh
+++ oh-my-zsh.zsh
@@ -11,7 +11,7 @@ ZSH_THEME="lambda-mod"
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=/home/hiep/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
