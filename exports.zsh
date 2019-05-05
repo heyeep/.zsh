4ec2a3bcca378507342d6beb67c98ee652beb6d0
Hiep Nguyen <yzmism@gmail.com>
2019-05-05 15:38:05 -0700

    Add nvm export

--- exports.zsh
+++ exports.zsh
@@ -63,3 +63,8 @@ export GOPATH=$HOME/Programming/go
 
 # https://stackoverflow.com/questions/18149601/go-install-always-fails-no-install-directory-outside-gopath
 export GOBIN=$GOPATH/bin
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
